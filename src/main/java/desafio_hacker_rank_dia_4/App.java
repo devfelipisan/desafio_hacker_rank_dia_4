131a7ac2cef42b00ce9945c267b3f7a7ea99208d
Felipi Torres Santana <devfelipisan@gmail.com>
2021-05-08 16:00:32 -0300

    Adicionando novo projeto desafio Hacker Rank dia 4
    Adicionando novo projeto desafio Hacker Rank dia 4

---
+++ src/main/java/desafio_hacker_rank_dia_4/App.java
@@ -0,0 +1,17 @@
+package desafio_hacker_rank_dia_4;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
